c550177dc8f81a52a5b911ae15f8a49170730f60
Zabolotny Wolodymyr <test_email@it.com>
2024-02-04 01:49:08 +0200

    Added sect 1 of Homework 7

--- src/hw07_20240123/MainHw07Sect1.java
+++ src/hw07_20240123/MainHw07Sect1.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
+/*1)
+Написать метод, принимающий в качестве параметра массив целых чисел. И выводит на экран все четные числа списком, a также нечетные числа списком.*/
 public class MainHw07Sect1 {
     public static void main(String[] args) {
         Random randomDigit = new Random();
